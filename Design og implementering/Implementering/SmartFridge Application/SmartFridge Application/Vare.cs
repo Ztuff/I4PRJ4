934fb2316f4c2877d8f9885564f3af92ede5df7e
201370747 <201370747@iha.dk>
2015-03-02 11:23:12 +0100

    Tilføjet vare klasse

---
+++ Design og implementering/Implementering/SmartFridge Application/SmartFridge Application/Vare.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFridge
+{
+    public class Item
+    {
+        private string _type;
+        private uint _amount;
+        private uint _size;
+        private string _unit;
+
+        public string Type { get { return _type; } }
+        public uint Amount { get { return _amount; } }
+        public uint Size { get { return _size; } }
+        public string Unit { get { return _unit; } }
+    }
+}
