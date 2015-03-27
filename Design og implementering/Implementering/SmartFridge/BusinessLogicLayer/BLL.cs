b3bf66c818fb0b5496d55451028f5143a7066613
201370747 <201370747@iha.dk>
2015-03-27 08:18:03 +0100

    Visual studio committede åbenbart ikke BLL

---
+++ Design og implementering/Implementering/SmartFridge/BusinessLogicLayer/BLL.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.AdoNetUoW;
+using DataAccessLayer.Connection;
+using DataAccessLayer.Repository;
+using InterfacesAndDTO;
+
+namespace BusinessLogicLayer
+{
+    public class BLL
+    {
+        private IConnectionFactory _connectionFactory = new AppConnectionFactory("SmartFridgeConn");
+        private IContext _context;
+        private ItemRepository _itemRepository;
+
+        public BLL()
+        {
+            _context = new AdoNetContext(_connectionFactory);
+            _itemRepository = new ItemRepository(_context);
+        }
+
+        public readonly List<string> UnitNames = new List<string>
+        {
+            "l",
+            "dl",
+            "cl",
+            "ml",
+            "kg",
+            "g"
+        };
+
+        public List<GUIItem> Types
+        {
+            get
+            {
+                List<Item> dbItems = _itemRepository.GetAll().ToList();
+                List<GUIItem> guiItems = new List<GUIItem>();
+
+                foreach (var dbItem in dbItems)
+                {
+                    GUIItem guiItem = new GUIItem();
+
+                    guiItem.Type = dbItem.ItemName;
+                    guiItem.Unit = dbItem.StdUnit;
+                    guiItem.Size = (uint)dbItem.StdVolume;
+
+                    guiItems.Add(guiItem);
+                }
+
+                return guiItems;
+            }
+        }
+
+        public GUIItem CreateNewItem(string type, uint amount, uint size, string unit)
+        {
+            GUIItem item = new GUIItem();
+            item.Type = type;
+            item.Amount = amount;
+            item.Size = size;
+            item.Unit = unit;
+            return item;
+        }
+
+
+    }
+}
