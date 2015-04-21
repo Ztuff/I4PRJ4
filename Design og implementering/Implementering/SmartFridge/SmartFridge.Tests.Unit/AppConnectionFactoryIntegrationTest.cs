f66d9af80f28accb98f7203e90f4d8e370682c8c
Thisen <thisen-dk@hotmail.com>
2015-04-21 15:48:48 +0200

    AppConnectionFactory tests

---
+++ Design og implementering/Implementering/SmartFridge/SmartFridge.Tests.Unit/AppConnectionFactoryIntegrationTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Connection;
+using NUnit.Framework;
+
+namespace SmartFridge.Tests.Unit
+{
+    [TestFixture]
+    class AppConnectionFactoryIntegrationTest
+    {
+        private AppConnectionFactory _uut;
+
+        [Test]
+        public void Ctor_ConnectionNameIsNull_ThrowsArgumentNullException()
+        {
+            Assert.That(() => _uut = new AppConnectionFactory(null), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Ctor_ConnectionNameIsTest_Throws_ConfigurationErrorsException()
+        {
+            Assert.That(() => _uut = new AppConnectionFactory("NotAValidString"), Throws.TypeOf<ConfigurationErrorsException>());
+        }
+
+        [Test]
+        public void Create_ConnectionNameIsSmartFridgeConn_ConnectionIsOpen()
+        {
+            _uut = new AppConnectionFactory("SmartFridgeConn");
+            IDbConnection connection = _uut.Create();
+            Assert.That(connection.ConnectionString, Is.EqualTo(@"Data Source=(localdb)\ProjectsV12;Initial Catalog=SmartFridge-SSDT;Integrated Security=True;Pooling=False;Connect Timeout=30"));
+        }
+
+    }
+}
