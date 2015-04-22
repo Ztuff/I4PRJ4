0b1db4a02541261fae00b27350adfd7fde755efe
Thisen <thisen-dk@hotmail.com>
2015-04-22 18:56:07 +0200

    Added AdoNetContextIntegrationTest.cs

---
+++ Design og implementering/Implementering/SmartFridge/SmartFridge.Tests.Unit/AdoNetContextIntegrationTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.AdoNetUoW;
+using DataAccessLayer.Connection;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace SmartFridge.Tests.Unit
+{
+    [TestFixture]
+    class AdoNetContextIntegrationTest
+    {
+        private AdoNetContext _uut;
+        [SetUp]
+        public void Setup()
+        {
+            var subConn = Substitute.For<IConnectionFactory>();
+            _uut = new AdoNetContext(subConn);
+        }
+
+    }
+}
