27a203bdc42a93743e02b8da36ce33806d80e137
Thisen <thisen-dk@hotmail.com>
2015-04-22 17:18:59 +0200

    AdoNetUoW tests

--- Design og implementering/Implementering/SmartFridge/SmartFridge.Tests.Unit/AdoNetUnitOfWorkIntegrationTest.cs
+++ Design og implementering/Implementering/SmartFridge/SmartFridge.Tests.Unit/AdoNetUnitOfWorkIntegrationTest.cs
@@ -18,11 +18,45 @@ namespace SmartFridge.Tests.Unit
         [SetUp]
         public void Setup()
         {
-            var transaction = Substitute.For<IDbTransaction>();
-            var action = Substitute.For<Action<AdoNetUnitOfWork>>();
-            _uut = new AdoNetUnitOfWork(transaction,action,action);
+            var subTransaction = Substitute.For<IDbTransaction>();
+            var subAction = Substitute.For<Action<AdoNetUnitOfWork>>();
+            _uut = new AdoNetUnitOfWork(subTransaction,subAction,subAction);
         }
 
-        
+        [Test]
+        public void Dispose_Called_DoesRollback()
+        {
+            _uut.Dispose();
+            _uut.Transaction.Received(1).Rollback();
+        }
+
+        [Test]
+        public void Dispose_Called_DoesDispose()
+        {
+            _uut.Dispose();
+            _uut.Transaction.Received(1).Dispose();
+        }
+
+        [Test]
+        public void Dispose_CalledTwiceAndTransactionIsNull_DoesNotRollBackTwice()
+        {
+            _uut.Dispose();
+            _uut.Dispose();
+            _uut.Transaction.Received(1).Rollback();
+        }
+
+        [Test]
+        public void SaveChanges_Called_DoesCommit()
+        {
+            _uut.SaveChanges();
+            _uut.Transaction.Received(1).Commit();
+        }
+
+        [Test]
+        public void SaveChanges_CalledTwice_ThrowsInvalidOperationException()
+        {
+            _uut.SaveChanges();
+            Assert.That(() => _uut.SaveChanges(),Throws.TypeOf<InvalidOperationException>());
+        }
     }
 }
