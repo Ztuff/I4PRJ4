31d4ca7ffa4bd5e17f592e71642e878afadecd37
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2015-04-22 18:04:55 +0200

    Test for 'AddItem' færdig.

--- Design og implementering/Implementering/SmartFridge/SmartFridge.Tests.Unit/BusinessLogicLayerTest.cs
+++ Design og implementering/Implementering/SmartFridge/SmartFridge.Tests.Unit/BusinessLogicLayerTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,15 +37,99 @@ namespace SmartFridge.Tests.Unit
             Assert.AreEqual(unit, item.Unit);
         }
 
-        [Test] //semi-integrationstest
+        [Test]
+        public void Compare_TwoSimilarItems_ReturnTrue()
+        {
+            var item1 = new GUIItem("Type", 1, 1, "Unit");
+            var item2 = new GUIItem("Type", 1, 1, "Unit");
+
+            var equality = uut.Compare(item1, item2);
+
+            Assert.AreEqual(true, equality);
+        }
+
+        [Test]
+        //Integrationstest
+        //Forudsætninger: 
+        // - At der er en database
+        // - At der er en liste der hedder 'Køleskab'
         public void AddItemsToTable_1NewItem_SameItemAddedToKøleskab()
         {
             //public void AddItemsToTable(string currentListName, ObservableCollection<GUIItem> newItems)
             var items = new ObservableCollection<GUIItem>();
-            items.Add(new GUIItem("Type", 1, 1,"Unit"));
-            uut.AddItemsToTable("Køleskab", items);
-            string bums = "";
+            var rnd = new Random();
+            string type = rnd.Next(int.MinValue, int.MaxValue).ToString();
+            items.Add(new GUIItem(type, 5, 1, "Unit"));
+            string listName = "Køleskab";
+            uut.AddItemsToTable(listName, items);
 
+            //Find the list...
+            GUIItemList list = null;
+            foreach (var guiItemList in uut.Lists)
+            {
+                if (guiItemList.Name.Equals("Køleskab"))
+                    list = guiItemList;
+            }
+            int EqualItems = 0;
+            foreach (var guiItem1 in items)
+            {
+                foreach (var guiItem2 in list.ItemList)
+                {
+                    if (uut.Compare(guiItem1, guiItem2))
+                        EqualItems++;
+                }
+            }
+
+            Assert.AreEqual(1,EqualItems);
+
+            foreach (var guiItem in items)
+            {
+                uut.DeleteItem(guiItem);
+            }
+        }
+
+        [Test]
+        //Integrationstest
+        //Forudsætninger: 
+        // - At der er en database
+        // - At der er en liste der hedder 'Køleskab'
+        public void AddItemsToTable_2NewItems_SameItemAddedsToKøleskab()
+        {
+            //public void AddItemsToTable(string currentListName, ObservableCollection<GUIItem> newItems)
+            var items = new ObservableCollection<GUIItem>();
+            var rnd = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+                string type = rnd.Next(int.MinValue, int.MaxValue).ToString();
+                items.Add(new GUIItem(type, 5, 1, "Unit"));
+            }
+            string listName = "Køleskab";
+            uut.AddItemsToTable(listName, items);
+
+            //Find the list...
+            GUIItemList list = null;
+            foreach (var guiItemList in uut.Lists)
+            {
+                if (guiItemList.Name.Equals("Køleskab"))
+                    list = guiItemList;
+            }
+            int EqualItems = 0;
+            foreach (var guiItem1 in items)
+            {
+                foreach (var guiItem2 in list.ItemList)
+                {
+                    if (uut.Compare(guiItem1, guiItem2))
+                        EqualItems++;
+                }
+            }
+
+            Assert.AreEqual(2, EqualItems);
+
+            foreach (var guiItem in items)
+            {
+                uut.DeleteItem(guiItem);
+            }
         }
 
     }
