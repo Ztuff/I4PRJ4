7e66b95ca36a2ebab16bdbd9eaafe1c7a80396e3
Thisen <thisen-dk@hotmail.com>
2015-04-26 15:04:53 +0200

    Sync databases, ready for test

--- Design og implementering/Implementering/SmartFridge/SmartFridgeDAL/Sync/DbSync.cs
+++ Design og implementering/Implementering/SmartFridge/SmartFridgeDAL/Sync/DbSync.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataAccessLayer.Connection;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Data;
+using Microsoft.Synchronization.Data.SqlServer;
+using Microsoft.Synchronization.Data.SqlServerCe;
 
 namespace DataAccessLayer.Sync
 {
     class DbSync
     {
-        private IConnectionFactory _serverConn;
-        private IConnectionFactory _clientConn;
+        private readonly IConnectionFactory _serverConn;
+        private readonly IConnectionFactory _clientConn;
+        private readonly string _sScope = "SmartFridgeScope";
 
         public DbSync(IConnectionFactory serverConn, IConnectionFactory clientConn)
         {
@@ -20,14 +26,62 @@ namespace DataAccessLayer.Sync
 
         public void ProvisionServer()
         {
-            _serverConn.Create();
+            SqlConnection serverConn = (SqlConnection) _serverConn.Create();
+            DbSyncScopeDescription scopeDesc = new DbSyncScopeDescription(_sScope);
+            
+            DbSyncTableDescription listDesc = SqlSyncDescriptionBuilder.GetDescriptionForTable("List", serverConn);
+            scopeDesc.Tables.Add(listDesc);
+            DbSyncTableDescription itemDesc = SqlSyncDescriptionBuilder.GetDescriptionForTable("Item", serverConn);
+            scopeDesc.Tables.Add(itemDesc);
+            DbSyncTableDescription listItemDesc = SqlSyncDescriptionBuilder.GetDescriptionForTable("ListItem", serverConn);
+            scopeDesc.Tables.Add(listItemDesc);
+
+            SqlSyncScopeProvisioning serverProvision = new SqlSyncScopeProvisioning(serverConn,scopeDesc);
+            serverProvision.SetCreateTableDefault(DbSyncCreationOption.Skip);
+
+            serverProvision.Apply();
         }
 
         public void ProvisionClient()
         {
-            _clientConn.Create();
+            SqlConnection clientConn = (SqlConnection)_clientConn.Create();
+            SqlConnection serverConn = (SqlConnection) _serverConn.Create();
+
+            DbSyncScopeDescription scopeDesc = SqlSyncDescriptionBuilder.GetDescriptionForScope(_sScope, serverConn);
+            SqlSyncScopeProvisioning clientProvision = new SqlSyncScopeProvisioning(clientConn,scopeDesc);
+
+            clientProvision.Apply();
         }
 
+        public void Sync()
+        {
+            SqlConnection clientConn = (SqlConnection)_clientConn.Create();
+            SqlConnection serverConn = (SqlConnection)_serverConn.Create();
+
+            SyncOrchestrator syncOrchestrator = new SyncOrchestrator();
+
+            syncOrchestrator.LocalProvider = new SqlSyncProvider(_sScope, clientConn);
+            syncOrchestrator.RemoteProvider = new SqlSyncProvider(_sScope,serverConn);
+
+            syncOrchestrator.Direction = SyncDirectionOrder.DownloadAndUpload;
+
+            ((SqlSyncProvider)syncOrchestrator.LocalProvider).ApplyChangeFailed += Program_ApplyChangeFailed;
+           
+            SyncOperationStatistics syncStats = syncOrchestrator.Synchronize();
+           
+            Console.WriteLine("Start Time: " + syncStats.SyncStartTime);
+            Console.WriteLine("Total Changes Uploaded: " + syncStats.UploadChangesTotal);
+            Console.WriteLine("Total Changes Downloaded: " + syncStats.DownloadChangesTotal);
+            Console.WriteLine("Complete Time: " + syncStats.SyncEndTime);
+            Console.WriteLine(String.Empty);
+        }
+ 
+        public void Program_ApplyChangeFailed(object sender, DbApplyChangeFailedEventArgs e)
+        {           
+            Console.WriteLine(e.Conflict.Type);
+            Console.WriteLine(e.Error);
+        }
+        }
 
 
     }
