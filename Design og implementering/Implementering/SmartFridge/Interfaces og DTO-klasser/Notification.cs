ae3cbc1f20734441de5e688dcb97a7c8e949f51c
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2015-04-30 11:06:17 +0200

    Initial commit

---
+++ Design og implementering/Implementering/SmartFridge/Interfaces og DTO-klasser/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAndDTO
+{
+    public class Notification
+    {
+        public string Message;
+        public DateTime TimeOfNotification;
+
+        public Notification(string message, DateTime time)
+        {
+            Message = message;
+            TimeOfNotification = time;
+        }
+    }
+}
