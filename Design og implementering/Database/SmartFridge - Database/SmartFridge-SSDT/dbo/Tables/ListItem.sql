697d8a0b8bd34be84eab1ad96020dcc7713ffab5
Thisen <thisen-dk@hotmail.com>
2015-05-07 13:36:00 +0200

    Added ShelfLife

--- Design og implementering/Database/SmartFridge - Database/SmartFridge-SSDT/dbo/Tables/ListItem.sql
+++ Design og implementering/Database/SmartFridge - Database/SmartFridge-SSDT/dbo/Tables/ListItem.sql
@@ -4,6 +4,7 @@
     [Amount] INT NOT NULL, 
     [Volume] INT NULL, 
     [Unit] NVARCHAR(MAX) NULL, 
+    [ShelfLife] DATETIME NULL, 
     CONSTRAINT [pk_ListItem] PRIMARY KEY CLUSTERED ([ListId] ASC, [ItemId] ASC),
     CONSTRAINT [fk_ListItem] FOREIGN KEY ([ListId]) REFERENCES [dbo].[List] ([ListId]) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT [fk_ListItem2] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Item] ([ItemId]) ON DELETE CASCADE ON UPDATE CASCADE
