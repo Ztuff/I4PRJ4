e0bc8b3f8624772be0560b54a7dafedf320c0855
Mat-Hapahapa <matry7@gmail.com>
2015-05-08 12:32:00 +0200

    Fixed "GuiItemList" I webApp

---
+++ Design og implementering/Implementering/SmartFridge_WebApplication/SmartFridge_WebApplication/Models/GUIItemList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SmartFridge_WebApplication.Models
+{
+    public class GUIItemList
+    {
+        public int ID { get; private set; }
+        public string Name;
+        public List<GUIItem> ItemList { get; private set; }
+
+        public GUIItemList(int id, string name)
+        {
+            Name = name;
+            ID = id;
+            ItemList = new List<GUIItem>();
+        }
+    }
+}
