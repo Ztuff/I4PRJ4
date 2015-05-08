838d49dd4cfc6ea10525cbce167c0d87ba83cf21
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2015-05-08 15:25:42 +0200

    Stylet grid 8-)

--- Design og implementering/Implementering/SmartFridge_WebApplication/SmartFridge_WebApplication/Controllers/LisViewController.cs
+++ Design og implementering/Implementering/SmartFridge_WebApplication/SmartFridge_WebApplication/Controllers/LisViewController.cs
@@ -11,17 +11,10 @@ namespace SmartFridge_WebApplication.Controllers
 {
     public class LisViewController : Controller
     {
-        //
-        // GET: /LisView/
-
-
-
         public ActionResult ListView()
         {
-            IEnumerable<GUIItem> model = new List<GUIItem>(){new GUIItem("KONTENT'SSSSS",1,1,"Reference")};
+            IEnumerable<GUIItem> model = new List<GUIItem>(){new GUIItem("KONTENT'SSSSS",1,1,"Reference"), new GUIItem("TreadsSS!", 1, 1, "Reference"){ShelfLife = new DateTime(2017,6,2)}};
             return View(model);
         }
-
-
     }
 }
