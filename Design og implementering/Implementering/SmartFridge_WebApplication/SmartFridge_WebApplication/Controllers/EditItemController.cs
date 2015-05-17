803969e01b4907daf3abf143a2c05fa995942071
201370747 <201370747@iha.dk>
2015-05-17 18:20:19 +0200

    commit to pull

--- Design og implementering/Implementering/SmartFridge_WebApplication/SmartFridge_WebApplication/Controllers/EditItemController.cs
+++ Design og implementering/Implementering/SmartFridge_WebApplication/SmartFridge_WebApplication/Controllers/EditItemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Mime;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
@@ -14,9 +15,14 @@ namespace SmartFridge_WebApplication.Controllers
 {
     public class EditItemController : Controller
     {
-        static private GUIItem _oldItem = new GUIItem();
-        static private GUIItemList _currentList;
-        static private ISmartFridgeDALFacade _dal;
+        private static GUIItem _oldItem = new GUIItem();
+        private static Models.List _currentList;
+        private static ISmartFridgeDALFacade _dal;
+        private static List<SelectListItem> _units;
+        private static List<SelectListItem> _types;
+        private static List<Item> _dbItems;
+        private static List<Models.ListItem> _dbListItems;
+        private static List<Models.List> _dbLists; 
 
         private GUIItem _updatedItem = new GUIItem();
 
@@ -29,9 +35,36 @@ namespace SmartFridge_WebApplication.Controllers
             _oldItem = new GUIItem("test", 1, 1, "l");//oldItem;
             /*_currentList = currentList;
             _dal = dal;*/
-            _currentList = new GUIItemList(0, "testlist");
             _dal = new SmartFridgeDALFacade();
+            _types = new List<SelectListItem>();
+            //var uow = _dal.GetUnitOfWork();
+            //_dbItems = uow.ItemRepo.GetAll().ToList();
+            //_dbListItems = uow.ListItemRepo.GetAll().ToList();
+            //_dbLists = uow.ListRepo.GetAll().ToList();
+            //_dal.DisposeUnitOfWork();
+            //foreach (var item in _dbItems)
+            //{
+            //    int value = 0;
+            //    _types.Add(new SelectListItem{Text = item.ItemName, Value = value.ToString()});
+            //    value++;
+            //}
+
+            //foreach (var list in _dbLists)
+            //{
+            //    if (list == TempData["CurrentListToEdit"])
+            //    {
+            //        _currentList = list;
+            //    }
+            //}
+
+            _units = new List<SelectListItem>(){new SelectListItem{Text = "l", Value = "0"},
+                                                new SelectListItem{Text = "dl", Value = "1"},
+                                                new SelectListItem{Text = "ml", Value = "2"},
+                                                new SelectListItem{Text = "kg", Value = "3"},
+                                                new SelectListItem{Text = "g", Value = "4"}};
             ViewData.Add("oldItem", _oldItem);
+            ViewBag.types = _types;
+            ViewBag.units = _units;
             return View();
         }
 
@@ -50,37 +83,44 @@ namespace SmartFridge_WebApplication.Controllers
         [HttpPost]
         public ActionResult UpdateItem(FormCollection collection)
         {
+            Item itemWithError = new Item();
 
             _updatedItem = new GUIItem(
                 collection["Type"],
                 Convert.ToUInt32(collection["Amount"]),
                 Convert.ToUInt32(collection["Volume"]),
                 collection["Unit"]
-                );
+                )/*{ShelfLife = Convert.ToDateTime(collection["Shelflife"])}*/;
 
             var uow = _dal.GetUnitOfWork();
-            List<SmartFridge_WebApplication.Models.ListItem> dbListItems = uow.ListItemRepo.GetAll().ToList();
-            List<Item> dbItems = uow.ItemRepo.GetAll().ToList();
-            foreach (var listItem in dbListItems)
+            foreach (var listItem in _dbListItems)
             {
-                if (listItem.Item.ItemId == _oldItem.ID && listItem.List.ListId == _currentList.ID)
+                if (listItem.Item.ItemId == _oldItem.ID && listItem.List.ListId == _currentList.ListId)
                 {
                     listItem.Amount = Convert.ToInt32(_updatedItem.Amount);
                     listItem.Volume = Convert.ToInt32(_updatedItem.Size);
                     listItem.Unit = _updatedItem.Unit;
                     if (listItem.Item.ItemName != _updatedItem.Type)
                     {
-                        foreach (var item in dbItems)
+                        foreach (var item in _dbItems)
                         {
+                            
+
+                            if (item.ItemName == _oldItem.Type)
+                            {
+                                itemWithError = item;
+                            }
                             if (item.ItemName == _updatedItem.Type)
                             {
                                 listItem.Item = item;
+                                uow.ItemRepo.Delete(itemWithError);
+                                itemWithError = null;
                             }
-                            else
-                            {
-                                var temp = new Item(_updatedItem.Type);
-                                uow.ItemRepo.Add(temp);
-                            }
+                        }
+                        if (itemWithError != null)
+                        {
+                            itemWithError.ItemName = _updatedItem.Type;
+                            uow.ItemRepo.Update(itemWithError);
                         }
                     }
                     uow.ListItemRepo.Update(listItem);
@@ -88,9 +128,9 @@ namespace SmartFridge_WebApplication.Controllers
                 }
             }
 
-            uow.SaveChanges();
+            //uow.SaveChanges();
 
-            _dal.DisposeUnitOfWork();
+            //_dal.DisposeUnitOfWork();
 
             //Jeg forestiller mig at strukturen bliver for fjern gammel 
             //indsæt ny bliver omtrent den samme, og at skellettet fra
