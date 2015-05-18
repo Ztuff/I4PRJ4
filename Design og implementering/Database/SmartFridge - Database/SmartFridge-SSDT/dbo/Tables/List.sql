30a6ed8b01ad3fea986fb77f5f611428ca8db835
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2015-05-18 14:24:01 +0200

    Tabellerne er nu i flertal.

--- Design og implementering/Database/SmartFridge - Database/SmartFridge-SSDT/dbo/Tables/List.sql
+++ Design og implementering/Database/SmartFridge - Database/SmartFridge-SSDT/dbo/Tables/List.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[List] (
+﻿CREATE TABLE [dbo].[Lists] (
     [ListId] INT IDENTITY (1, 1) NOT NULL,
     [ListName] NVARCHAR(MAX) NOT NULL, 
     CONSTRAINT [pk_List] PRIMARY KEY CLUSTERED ([ListId] ASC)
