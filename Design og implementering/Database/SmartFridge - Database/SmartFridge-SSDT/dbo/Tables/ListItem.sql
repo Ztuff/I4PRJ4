3fefb82ab196015fb0c8989b01ca74bd4b06e100
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2015-05-20 10:07:17 +0200

    Superkey tilføjet, skulle gerne rette bugs ud.

--- Design og implementering/Database/SmartFridge - Database/SmartFridge-SSDT/dbo/Tables/ListItem.sql
+++ Design og implementering/Database/SmartFridge - Database/SmartFridge-SSDT/dbo/Tables/ListItem.sql
@@ -2,10 +2,10 @@
     [ListId] INT NOT NULL,
     [ItemId] INT NOT NULL,
     [Amount] INT NOT NULL, 
-    [Volume] INT NULL, 
+    [Volume] INT NOT NULL, 
     [Unit] NVARCHAR(MAX) NULL, 
-    [ShelfLife] DATETIME NULL, 
-    CONSTRAINT [pk_ListItems] PRIMARY KEY CLUSTERED ([ListId] ASC, [ItemId] ASC),
+    [ShelfLife] DATETIME NOT NULL, 
+    CONSTRAINT [pk_ListItems] PRIMARY KEY CLUSTERED ([ShelfLife], [Volume], [Amount], [ItemId], [ListId]),
     CONSTRAINT [fk_ListItems] FOREIGN KEY ([ListId]) REFERENCES [dbo].[Lists] ([ListId]) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT [fk_ListItems2] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Items] ([ItemId]) ON DELETE CASCADE ON UPDATE CASCADE
 );
