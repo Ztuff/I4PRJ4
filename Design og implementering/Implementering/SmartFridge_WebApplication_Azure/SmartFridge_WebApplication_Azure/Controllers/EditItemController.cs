2ba7a24999bd39e821dd61ad398f4d265a7087d4
201370747 <201370747@iha.dk>
2015-05-21 08:30:03 +0200

    Rettet så Index sender de rigtige liste navne med, og EditItem kan nu redigere items, men crasher ved opdatering til i forvejen eksisterende item.

--- Design og implementering/Implementering/SmartFridge_WebApplication_Azure/SmartFridge_WebApplication_Azure/Controllers/EditItemController.cs
+++ Design og implementering/Implementering/SmartFridge_WebApplication_Azure/SmartFridge_WebApplication_Azure/Controllers/EditItemController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web.Mvc;
 using SmartFridge_WebDAL;
@@ -17,7 +18,7 @@ namespace SmartFridge_WebApplication.Controllers
         private static List<SelectListItem> _types;
         private static List<Item> _dbItems;
         private static List<ListItem> _dbListItems;
-        private static List<List> _dbLists; 
+        private static List<List> _dbLists;
 
         private GUIItem _updatedItem = new GUIItem();
 
@@ -32,7 +33,7 @@ namespace SmartFridge_WebApplication.Controllers
             _dal = dal;*/
             _dal = new SmartFridgeDALFacade();
 
-            _oldItem = new GUIItem("test", 1, 1, "dl");//oldItem;
+            _oldItem = oldItem;
             _dal = new SmartFridgeDALFacade("SmartFridgeDb");
 
             _types = new List<SelectListItem>();
@@ -41,12 +42,10 @@ namespace SmartFridge_WebApplication.Controllers
             _dbListItems = uow.ListItemRepo.GetAll().ToList();
             _dbLists = uow.ListRepo.GetAll().ToList();
             _dal.DisposeUnitOfWork();
-            _types.Add(new SelectListItem { Text = "Varetype", Value = "Varetype", Selected = true});
-            int value = 0; 
+            _types.Add(new SelectListItem { Text = "Varetype", Value = "Varetype", Selected = true });
             foreach (var item in _dbItems)
             {
-                _types.Add(new SelectListItem { Text = item.ItemName, Value = value.ToString() });
-                value++;
+                _types.Add(new SelectListItem { Text = item.ItemName, Value = item.ItemName });
             }
 
             foreach (var list in _dbLists)
@@ -54,13 +53,16 @@ namespace SmartFridge_WebApplication.Controllers
                 if (list.ListName == TempData.Peek("CurrentListToEdit").ToString())
                 {
                     _currentList = list;
+                    TempData.Keep("CurrentListToEdit");
+                    break;
                 }
             }
             _units = new List<SelectListItem>(){new SelectListItem{Text = "l", Value = "l"},
                                                 new SelectListItem{Text = "dl", Value = "dl"},
                                                 new SelectListItem{Text = "ml", Value = "ml"},
                                                 new SelectListItem{Text = "kg", Value = "kg"},
-                                                new SelectListItem{Text = "g", Value = "g"}};
+                                                new SelectListItem{Text = "g", Value = "g"},
+                                                new SelectListItem{Text = "stk", Value = "stk"}};
             ViewData.Add("oldItem", _oldItem);
             ViewBag.types = _types;
             ViewBag.units = _units;
@@ -70,19 +72,21 @@ namespace SmartFridge_WebApplication.Controllers
         [HttpPost]
         public void Increment()
         {
-   
+
         }
 
         [HttpPost]
         public void Decrement()
         {
-            
+
         }
         /// <summary>
-        /// 
+        /// Updates the selected item. If the item is updated in such a way that Type, ShelfLife, unit and size are similar to an already existing item,
+        /// the amount of the existing item becomes the sum of its previous amount and the amount of the updated item. If this is the case the updated 
+        /// item will be deleted.
         /// </summary>
         /// <param name="collection"> Collection of input from HTML form</param>
-        /// <returns></returns>
+        /// <returns> Returns a redirect to the ListView</returns>
         [HttpPost]
         public ActionResult UpdateItem(FormCollection collection) //Mangler tjek på om den pågældene list item eksistere i forvejen
         {
@@ -94,9 +98,9 @@ namespace SmartFridge_WebApplication.Controllers
                 Convert.ToUInt32(collection["Volume"]),
                 collection["units"]
                 );
-            if(date.Length == 0)
-            {             
-                _updatedItem.ShelfLife = default(DateTime) /*Convert.ToDateTime(collection["Shelflife"])*/;
+            if (date.Length == 0)
+            {
+                _updatedItem.ShelfLife = default(DateTime);
             }
             else
             {
@@ -107,6 +111,7 @@ namespace SmartFridge_WebApplication.Controllers
                 if (item.ItemName == _oldItem.Type)
                 {
                     itemWithError = item;
+                    break;
                 }
             }
             var uow = _dal.GetUnitOfWork();
@@ -124,8 +129,25 @@ namespace SmartFridge_WebApplication.Controllers
                             if (item.ItemName == _updatedItem.Type)
                             {
                                 listItem.Item = item;
-                                uow.ItemRepo.Delete(itemWithError);
                                 itemWithError = null;
+                                foreach (var duplicateItem in _dbListItems)
+                                {
+                                    if (duplicateItem.Item.ItemId == listItem.Item.ItemId && //Tjek på om der allerede eksistere en tilsavrende vare
+                                        duplicateItem.List.ListId == listItem.List.ListId && //gør der det opdateres den eksisterende vares antal
+                                        duplicateItem.Unit == listItem.Unit &&               //med antallet fra den opdaterede vare og den 
+                                        duplicateItem.Volume == listItem.Volume &&           //opdaterede vare slettes
+                                        duplicateItem.ShelfLife == listItem.ShelfLife)
+
+                                    {
+                                        duplicateItem.Amount += listItem.Amount;
+                                        uow.ListItemRepo.Update(duplicateItem);
+                                        uow.ListItemRepo.Delete(listItem);
+                                        uow.SaveChanges();
+                                        _dal.DisposeUnitOfWork();
+
+                                        return RedirectToAction("ListView", "LisView");
+                                    }
+                                }
                             }
                         }
                         if (itemWithError != null)
@@ -138,6 +160,7 @@ namespace SmartFridge_WebApplication.Controllers
                     break;
                 }
             }
+            uow.SaveChanges();
             _dal.DisposeUnitOfWork();
 
             return RedirectToAction("ListView", "LisView");
