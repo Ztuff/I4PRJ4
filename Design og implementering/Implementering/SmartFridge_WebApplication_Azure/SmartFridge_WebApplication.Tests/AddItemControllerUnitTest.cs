6cd6dbc5e0188ce027f66cc1539e3cf460232186
Mathias Siig Nørregaard <mathias_sn@hotmail.com>
2015-05-26 13:54:24 +0200

    Prøvet at teste AddItem :S

--- Design og implementering/Implementering/SmartFridge_WebApplication_Azure/SmartFridge_WebApplication.Tests/AddItemControllerUnitTest.cs
+++ Design og implementering/Implementering/SmartFridge_WebApplication_Azure/SmartFridge_WebApplication.Tests/AddItemControllerUnitTest.cs
@@ -4,14 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using NSubstitute;
 using NUnit.Framework;
+using SmartFridge_Cache;
 using SmartFridge_WebApplication.Controllers;
+using SmartFridge_WebDAL;
+using SmartFridge_WebModels;
+using List = NUnit.Framework.List;
 
 namespace SmartFridge_WebApplication.Tests
 {
     [TestFixture]
-    class AddItemControllerUnitTest
+    public class AddItemControllerUnitTest
     {
+        private AddItemController uut;
+        [SetUp]
+        public void Setup()
+        {
+            uut = new AddItemController();
+            var dalfacade = Substitute.For<ISmartFridgeDALFacade>();
+            Cache.DalFacade = dalfacade;
+            var currentList = new SmartFridge_WebModels.List("TestList") { ListId = 0 };
+            Cache.CurrentList = currentList;
+            Item item = new Item("TestType") { ItemId = 1 };
+            ListItem listitem = new ListItem(1, 1, "l", default(DateTime), null, item) { ItemId = item.ItemId };
+            dalfacade.GetUnitOfWork().ListRepo.GetAll().Returns(new List<SmartFridge_WebModels.List>() { currentList });
+            dalfacade.GetUnitOfWork().ItemRepo.GetAll().Returns(new List<Item> { item });
+            dalfacade.GetUnitOfWork().ListItemRepo.GetAll().Returns(new List<ListItem>() { listitem });
+        }
+
+        [Test]
+        public void addNewItem_Add1ItemAndExit_LisViewReturned()
+        {
+            RedirectToRouteResult result = uut.addNewItem("TestItem", "1", "1", "l", "06-09-1993", "Exit") as RedirectToRouteResult;
+            Assert.That(result.RouteValues.Values.ElementAt(1), Is.EqualTo("LisView"));
+        }
+
+        [Test]
+        public void addNewItem_Add1ItemAndDontExit_AddItemReturned()
+        {
+            var result = uut.addNewItem("TestItem", "1", "1", "l", "06-09-1993", "no") as PartialViewResult;
+            Assert.That(result.ViewName,Is.EqualTo("AddItem"));
+        }
+
+        [Test]
+        public void AddItem_TestLoadingCorrectModel_ReturnCorrectModel()
+        {
+
+            var additem = uut.AddItem() as ViewResult;
+            Assert.AreEqual(null, additem);
+        }
 
         [Test]
         public void Exit_NoNewItems_ReturnNull()
